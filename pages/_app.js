2550d16f97fb0e91718f937de29845087c930c2c
Duydt <duongduyy1512@gmail.com>
2022-09-02 17:51:34 +0700

    initial

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import { Toaster } from "react-hot-toast";
+import { Layout } from "../components";
+import { StateContext } from "../context/StateContext";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
+}
+
+export default MyApp;
